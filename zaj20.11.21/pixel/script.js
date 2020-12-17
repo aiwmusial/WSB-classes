8ee99620de0c041105da7d3358a8d06fa096da48
aiwmusial <53020292+aiwmusial@users.noreply.github.com>
2020-12-17 10:17:59 +0000

    Add files via upload
    A grid of pixels can be coloured by the colours chosen from the palette provided. This upload contains an easy JavaScript code, as well as HTML and CSS files.

---
+++ zaj20.11.21/pixel/script.js
@@ -0,0 +1,10 @@
+// function setPixelColor(pixel){
+//     pixel.style.backgroundColor = 'black';
+// }
+// var penColour = 'black';
+function  setPenColour(pen){
+    penColour = pen;
+}
+function setPixelColor(pixel){
+    pixel.style.backgroundColor = penColour;
+}
